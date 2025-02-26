7660e41a6395ec32d38362710692aabea8714d16
pranshukatyal14 <pranshukatyal62@gmail.com>
2025-02-26 13:53:39 +0530

    added changes

--- Foundation/l001.java
+++ Foundation/l001.java
@@ -7,7 +7,7 @@ public class l001{
         System.out.println("This is pranshu katyal");
     }
     
-    public static workWithDataType(){
+    public static void workWithDataType(){
         int a=10;
         int b=20;
         System.out.println("value of a is "+ a);
@@ -36,9 +36,9 @@ public class l001{
     }
     public static void oddEven(int n){
         if(n%2==0){
-            System.out.println("Even")
+            System.out.println("Even");
         }else{
-            System.out.println("Odd")
+            System.out.println("Odd");
 
         }
     }
@@ -56,9 +56,61 @@ public class l001{
         int n=scn.nextInt();
         boolean ans=isPrime_(n);
         if(ans){
-            System.out.println("Prime")
+            System.out.println("Prime");
         }else{
-            System.out.println("Not Prime")
+            System.out.println("Not Prime");
         }
+    }
+    public static void printAllPrime(int a,int b){
+        for(int i=a;i<=b;i++){
+            boolean ans=isPrime_(i);
+            if(ans){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void printFibo(int n){
+        int a=0;
+        int b=1;
+        for(int i=1;i<=n;i++){
+            System.out.println(a);
+            int sum=a+b;
+            a=b;
+            b=sum;
+        }
+    }
+    public static int countDigits(int n){
+        int count=0;
+        int num=n;
+        while(num!=0){
+            num=num/10;
+            count++;
+        }
+        return count;
+    }
+    public static void lineWiseDigits(int n){
+        int num=n;
+        int len=countDigits(num);
+        int div=1;
+        while(len -- >1){
+            div=div*10;
+        }
+        while(div!=0){
+            System.out.println(n/div);
+            n=n%div;
+            div=div/10;
+        }
+    }
+    public static int reverse(int n){
+        int res=0;
+        for(int i=1;n!=0;i++){
+            int digit=n%10;
+            n=n/10;
+            res+=i* Math.pow(10,digit-1);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        printHello();
     }
 }
