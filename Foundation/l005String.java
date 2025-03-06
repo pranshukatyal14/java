25436aef939f7a5c24c987a0b15495be4c107fbf
pranshukatyal14 <pranshukatyal62@gmail.com>
2025-03-07 00:53:35 +0530

    added string code

--- Foundation/l005String.java
+++ Foundation/l005String.java
@@ -137,6 +137,132 @@ public class l005String{
     //         ans=smallAns;
     //     }
     // }
+
+    public static boolean isplaindrome(String str){
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i++)!=str.charAt(j--)){
+                return false;
+            }
+        
+        }
+        return true;
+    }
+    public static void allSubString(String str){
+        for(int i=0;i<str.length;i++){
+            for(int len=1;i+len<=str.length();len++){
+                System.out.println(str.substring(i,i+len));
+            }
+        }
+    }
+    
+    public static String compression1(String str){
+        if(str.length()<=1){
+            return str;
+        }
+        StringBuilder sb=new StringBuilder();
+        sb.append(str.charAt(0));
+        int i=1;
+        while(i<str.length()){
+            while(i<str.length && str.charAt(i-1)==str.charAt(i)){
+                i++
+            }
+            if(i<str.length()){
+                sb.append(str.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String compression2(String str){
+        if(str.length<=1){
+            return str;
+        }
+        StringBuilder sb=new StringBuilder();
+        sb.append(str.charAt(0));
+        int i=1;
+        while(i<str.length()){
+            int count=1;
+            while(i<str.length() && str.charAt(i-1)==str.char(i)){
+                count++;
+                i++;
+            }
+            if(count!=1){
+                sb.append(count);
+            }
+            if(i<str.length()){
+                sb.append(str.charAt(i))
+            }
+        }
+        return sb.toString();
+    }
+    public static String withoutX2(String str){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(i<2 && str.charAt(i)!='x' ){
+                sb.append(str.charAt(i));
+            }
+            if(i>=2){
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    // toggle case
+    public static String toggleCase(String str){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch>="a" && ch<="z"){
+                sb.append((char)(ch-"a"+"A"))
+            }else{
+                sb.append((char)(ch-"A"+"a"))
+            }
+        }
+        return sb.toString();
+    }
+    // string with difference of every two consecutive characters
+    public static String solution(String str){
+        if(str.length()<=1){
+            return str;
+        }
+        StringBuilder sb=new StringBuilder();
+        sb.append(str.charAt(0));
+        for(int i=1;i<str.length();i++){
+            int diff=str.charAt(i)-str.charAt(i-1);
+            char ch=str.charAt(i);
+            sb.append(diff);
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+    public static ArrayList<Integer> occuOfChar(String str,char ch){
+        ArrayList<Integer>arr=new ArrayList<>();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==ch){
+                arr.add(i);
+            }
+        }
+        return arr;
+    }
+
+    public static ArrayList<String> subSeq(String str){
+        ArrayList<String>ans=new ArrayList<>();
+        ans.add("");
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            int size=ans.size();
+            for(int j=0;i<size;i++){
+                ans.add(ans.get(j)+ch)
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
         System.out.println("working fine");
     }
