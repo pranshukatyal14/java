0a1672893f6aa8b03ca86a129a46a5f449d32d21
pranshukatyal14 <pranshukatyal62@gmail.com>
2025-03-09 12:50:49 +0530

    working on online questions

--- pepCodingOnline/level1/l001.java
+++ pepCodingOnline/level1/l001.java
@@ -22,7 +22,7 @@ public class l001{
             boolean flag=((a*a)+(b*b))==(c*c);
             return flag;
         }
-
+        return false;
     }
     public static void benjaminBulbs(int n){
         // find the perfect square is the solution
@@ -30,7 +30,50 @@ public class l001{
             System.out.println(i*i);
         }
     }
+    public static int spanOfArray(int[] arr){
+        // find max and min then calculate the difference
+        int maxEle=arr[0];
+        int minEle=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(maxEle<arr[i]){
+                maxEle=arr[i];
+            }
+            if(minEle>arr[i]){
+                minEle=arr[i];
+            }
+        }
+        return maxEle-minEle;
+    }
+    public static int findEleinArray(int[]arr,int data){
+        // return index
+        int index=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==data){
+                return index;
+            }
+        }
+        return index;
+    }
+    public static void barChart(int[]arr){
+      int max=arr[0];
+      for(int i=1;i<arr.length;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+      }
+      for(int floor=max;floor>=1;floor--){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>=floor){
+               System.out.print("*\t"); 
+            }else{
+                System.out.print("\t");
+            }
+        }
+        System.out.println();
+      }
+    }
     public static void main(String[] args){
-        
+        int[] arr=new int[]{1,3,5,2,5};
+        barChart(arr);
     }
 }
